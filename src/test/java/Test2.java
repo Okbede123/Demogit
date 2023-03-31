74606421b4fd4beda65af5e887f9b95929b40036
Hung Phat <vuminhduc24081997@gmail.com>
2023-03-31 22:58:20 +0700

    test nhanh laptop master 3

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,7 +3,7 @@ public class Test2 implements TestInterFace {
 
     @Override
     public void Tc_01() {
-        System.out.println("test nhanh laptop master 10h53");
+        System.out.println("test nhanh laptop master 10h57");
 
         System.out.println("dong 2 laptop");
     }
