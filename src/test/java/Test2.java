ed4617804af701801a4afc93eb695d5b56aebf1b
Hung Phat <vuminhduc24081997@gmail.com>
2023-04-01 11:20:23 +0700

    test nhanh laptop 1.0

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,16 +3,16 @@ public class Test2 implements TestInterFace {
 
     @Override
     public void Tc_01() {
-        System.out.println("dong 1 nhanh pc_2_5");
+        System.out.println("dong 1 nhanh laptop_1_0");
     }
 
     @Override
     public int Tc_02() {
-        return 25;
+        return 1;
     }
 
     @Override
     public void testInterface2() {
-        System.out.println("dong 2 nhanh pc_2_5");
+        System.out.println("dong 2 nhanh laptop_1_0");
     }
 }
