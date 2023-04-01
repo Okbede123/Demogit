24a99ae79bcf6f84d26af4bd0fa8d7f14d7b96e0
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-04-01 11:01:29 +0700

    nhanh test lan 15

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -1,7 +1,7 @@
 public class Classthu2DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("test 1  nhanh lan 14");
-        System.out.println("test 2 nhanh lan 14");
+        System.out.println("test 1  nhanh lan 15");
+        System.out.println("test 2 nhanh lan 15");
     }
 }
