8e42b8c23f1e7d3d6a95c87768e895ddf2b0f44b
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-04-01 10:58:57 +0700

    nhanh test lan 14

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,18 +3,18 @@ public class Test2 implements TestInterFace {
 
     @Override
     public void Tc_01() {
-        System.out.println("test nhanh master lan 13");
+        System.out.println("test nhanh master lan 14");
 
     }
 
     @Override
     public int Tc_02() {
-        return 13;
+        return 14;
     }
 
     @Override
     public void testInterface2() {
-        System.out.println("dong 2 nhanh master lan 13");
+        System.out.println("dong 2 nhanh master lan 14");
 
     }
 }
