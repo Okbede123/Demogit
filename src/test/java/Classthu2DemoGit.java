88af39154320d2e4b326766311430f87aaf4eb61
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-04-01 10:29:33 +0700

    nhanh test master 9

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -1,6 +1,6 @@
 public class Classthu2DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("test  nhanh lan 8");
+        System.out.println("test  nhanh lan 9");
     }
 }
