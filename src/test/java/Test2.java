ce1d73874ea512c13c5b374907d0c2140e852069
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-04-01 09:49:13 +0700

    Revert "nhanh test nhanh pc 1/4 4"
    This reverts commit a4a3f68b4b285069c48062a816a6bebb2b2ed2fd.

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,27 +3,20 @@ public class Test2 implements TestInterFace {
 
     @Override
     public void Tc_01() {
-        System.out.println("test nhanh laptop 9h38");
+        System.out.println("1/4 9h36");
 
         System.out.println("dong 2 laptop");
-
-        System.out.println("9h38");
     }
 
     @Override
     public int Tc_02() {
-        return 5;
+        return 0;
     }
 
     @Override
     public void testInterface2() {
         System.out.println("dong 2 nhanh laptop 2");
-        System.out.println("9h38");
-        System.out.println("them 1 dong nua");
-        System.out.println("dong nhanh pc");
         System.out.println("9h36 nhe");
         System.out.println("them dong nua");
-        System.out.println(" dong nua pc");
-
     }
 }
