aec71aa21f1f283c200202d0746477112d2839c0
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-04-01 10:14:51 +0700

    nhanh test pc 9 chuan pc

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,20 +3,19 @@ public class Test2 implements TestInterFace {
 
     @Override
     public void Tc_01() {
-        System.out.println("dong 1 nhanh pc_1_4");
-        System.out.println("test nhanh master lan 5");
+        System.out.println("dong 1 nhanh pc_1_5");
 
     }
 
     @Override
     public int Tc_02() {
-        return 3+13;
+        return 4;
     }
 
     @Override
     public void testInterface2() {
-        System.out.println("dong 2 nhanh pc_1_4");
-        System.out.println("dong 2 nhanh master lan 5");
+        System.out.println("dong 2 nhanh pc_1_5");
+
 
     }
 }
