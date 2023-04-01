60f4abcd3aac255165b61681cbd308a4b6605ff0
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-04-01 11:00:51 +0700

    nhanh test pc 2.5

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -1,7 +1,7 @@
 public class Classthu2DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("dong 1 nhanh pc_2_4");
-        System.out.println("dong 2 nhanh pc_2_4");
+        System.out.println("dong 1 nhanh pc_2_5");
+        System.out.println("dong 2 nhanh pc_2_5");
     }
 }
--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,16 +3,16 @@ public class Test2 implements TestInterFace {
 
     @Override
     public void Tc_01() {
-        System.out.println("dong 1 nhanh pc_2_4");
+        System.out.println("dong 1 nhanh pc_2_5");
     }
 
     @Override
     public int Tc_02() {
-        return 24;
+        return 25;
     }
 
     @Override
     public void testInterface2() {
-        System.out.println("dong 2 nhanh pc_2_4");
+        System.out.println("dong 2 nhanh pc_2_5");
     }
 }
