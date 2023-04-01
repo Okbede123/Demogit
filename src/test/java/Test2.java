c8bef9d9b6b850686416aad2f80664258a1277e5
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-04-01 11:24:57 +0700

    nhanh test lan 2.6

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,16 +3,16 @@ public class Test2 implements TestInterFace {
 
     @Override
     public void Tc_01() {
-        System.out.println("dong 1 nhanh pc_2_5");
+        System.out.println("dong 1 nhanh pc_2_6");
     }
 
     @Override
     public int Tc_02() {
-        return 25;
+        return 26;
     }
 
     @Override
     public void testInterface2() {
-        System.out.println("dong 2 nhanh pc_2_5");
+        System.out.println("dong 2 nhanh pc_2_6");
     }
 }
