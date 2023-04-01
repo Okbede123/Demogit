040a69650454e32330bab17aa9a94af4bff90de0
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-04-01 10:24:53 +0700

    nhanh test pc 1.7

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -1,6 +1,6 @@
 public class Classthu2DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("test  nhanh pc 1.6");
+        System.out.println("test  nhanh pc 1.7");
     }
 }
