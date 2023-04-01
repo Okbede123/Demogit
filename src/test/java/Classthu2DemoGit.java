426bc09a0945a0988d271f953d328fc99d0fb6f9
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-04-01 10:52:04 +0700

    nhanh test pc2.3

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -1,6 +1,7 @@
 public class Classthu2DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("test  nhanh lan 12");
+        System.out.println("dong 1 nhanh pc_2_3");
+        System.out.println("dong 2 nhanh pc_2_3");
     }
 }
--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,17 +3,16 @@ public class Test2 implements TestInterFace {
 
     @Override
     public void Tc_01() {
-        System.out.println("dong 1 nhanh pc_2_1");
+        System.out.println("dong 1 nhanh pc_2_3");
     }
 
     @Override
     public int Tc_02() {
-        return 12;
+        return 23;
     }
 
     @Override
     public void testInterface2() {
-        System.out.println("dong 2 nhanh master lan 12");
-
+        System.out.println("dong 2 nhanh pc_2_3");
     }
 }
