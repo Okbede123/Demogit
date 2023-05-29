177233c676f64b4fe197e296b8819052f6454f54
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-05-29 20:59:23 +0700

    nhanh 5

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -1,7 +1,7 @@
 public class Classthu2DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("test 29/05-4 laptop");
-        System.out.println("29/05/ 4.1 laptop");
+        System.out.println("test 29/05-5 laptop");
+        System.out.println("29/05/ 5.1 laptop");
     }
 }
