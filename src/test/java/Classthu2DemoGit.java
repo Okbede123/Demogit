01b907d4116152d03eaec732660f5ecf9b00cc16
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-05-29 22:07:18 +0700

    35 sao luu 1

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -1,10 +1,10 @@
 public class Classthu2DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("test 29/05-33 lap");
-        System.out.println("29/05/ 33.1 lap");
-        System.out.println("29/05/ 33.2 lap");
-        System.out.println("29/05/ 33.3 lap");
+        System.out.println("test 29/05-34 pc");
+        System.out.println("29/05/ 34.1 pc");
+        System.out.println("29/05/ 34.2 pc");
+        System.out.println("29/05/ 34.3 pc");
     }
 
     public void tc1(){
