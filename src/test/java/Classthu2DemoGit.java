ee4e9a6cbc56e1a81d969cca01002bd325ff67b8
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-05-29 20:53:25 +0700

    nhanh 2

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -5,6 +5,6 @@ public class Classthu2DemoGit {
         System.out.println("dong 2 nhanh laptop_1_1");
         System.out.println("dong 1 nhanh pc_2_7");
         System.out.println("dong 2 nhanh pc_2_7");
-        System.out.println("test 29/05-1");
+        System.out.println("test 29/05-2");
     }
 }
