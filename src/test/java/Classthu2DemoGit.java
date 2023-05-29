7dfeed9ae17d3dbbf8c851d89a8b1f297e6e916f
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-05-29 21:02:33 +0700

    nhanh 8

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -1,7 +1,9 @@
 public class Classthu2DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("test 29/05-7 laptop");
-        System.out.println("29/05/ 7.1 laptop");
+        System.out.println("test 29/05-8 laptop");
+        System.out.println("29/05/ 8.1 laptop");
+
+        System.out.println("29/05/ 8.2 laptop");
     }
 }
