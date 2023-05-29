4b1e221dc7dc9ace2685a3eb3a5e2f4aa17f1e15
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-05-29 21:10:27 +0700

    nhanh 13

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -1,11 +1,11 @@
 public class Classthu2DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("test 29/05-13 lap");
-        System.out.println("29/05/ 13.1 lap");
+        System.out.println("test 29/05-14 lap");
+        System.out.println("29/05/ 14.1 lap");
 
-        System.out.println("29/05/ 13.2 lap");
+        System.out.println("29/05/ 14.2 lap");
 
-        System.out.println("29/05/ 13.3 lap");
+        System.out.println("29/05/ 14.3 lap");
     }
 }
