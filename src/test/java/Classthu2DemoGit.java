afb8b7857843afe3ba1ff558c885469ad573da98
Hung Phat <vuminhduc24081997@gmail.com>
2023-05-29 21:37:48 +0700

    nhanh 24

--- src/test/java/Classthu2DemoGit.java
+++ src/test/java/Classthu2DemoGit.java
@@ -1,10 +1,10 @@
 public class Classthu2DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("test 29/05-22 lap");
-        System.out.println("29/05/ 22.1 lap");
-        System.out.println("29/05/ 22.2 lap");
-        System.out.println("29/05/ 22.3 lap");
-        System.out.println("29/05/ 22.4 lap");
+        System.out.println("test 29/05-23 lap");
+        System.out.println("29/05/ 23.1 lap");
+        System.out.println("29/05/ 23.2 lap");
+        System.out.println("29/05/ 23.3 lap");
+        System.out.println("29/05/ 23.4 lap");
     }
 }
